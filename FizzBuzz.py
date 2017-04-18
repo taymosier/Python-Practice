bbce12d43271bfc363a5eac279c1bc37a85d7c25
Taylor Mosier <taymosier@gmail.com>
2017-04-18 16:16:56 -0400

    Add files via upload
    Classic FizzBuzz problem.

---
+++ FizzBuzz.py
@@ -0,0 +1,22 @@
+from random import randint
+
+input = randint
+
+print("The number is " + str(input))
+
+if(input % 3 == 0) and (input % 5 == 0):
+    {
+        print("Fizzbuzz")
+    }
+elif(input %3 == 0):
+    {
+        print("Fizz")
+    }
+elif(input % 5 == 0):
+    {
+        print("Buzz")
+    }
+else:
+    {
+        print("This number is not divisible by 3 or 5")
+    }
